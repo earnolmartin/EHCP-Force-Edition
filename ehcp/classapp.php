f1add173623d0b9b6870435ca22ce8ae4f20ac62
earnolmartin <earnolmartin@gmail.com>
2019-01-23 16:17:20 -0700

    Better Cleanup for Let's Encrypt

--- ehcp/classapp.php
+++ ehcp/classapp.php
@@ -5597,10 +5597,10 @@ function deleteFileFromSystem($file, $recursive = false){
 }
 
 function runCommandInDaemon($command){
-	return $this->addDaemonOp('runsystemcommand', $command, '', '', 'Run System Command');
+	return $this->addDaemonOp('runsystemcommand', $command, 1, '', 'Run System Command');
 }
 
-function runSystemCommand($command){
+function runSystemCommand($command, $info = ''){
 	$this->requireCommandLine(__FUNCTION__);
 	echo "Running system command of \"" . $command . "\"" . "\n";
 	if(isset($command) && !empty($command)){
@@ -14321,7 +14321,7 @@ function runop2($op,$action,$info,$info2='',$info3=''){
 			return $this->deleteFileFromSystem($action,$info);
 			break;
 		case 'runsystemcommand':
-			return $this->runSystemCommand($action);
+			return $this->runSystemCommand($action, $info);
 			break;
 		case 'daemon_backup_domain':
 			return $this->daemon_backup_domain($info);
