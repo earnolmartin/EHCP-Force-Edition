e989f9b93d9b8ff36316ad89db81c310867339a0
earnolmartin <earnolmartin@gmail.com>
2019-04-30 15:59:49 -0600

    Minor Fix

--- ehcp/ehcpforceupdate.sh
+++ ehcp/ehcpforceupdate.sh
@@ -2914,7 +2914,7 @@ function installBadBotsBlockerNginx(){
 			git clone https://github.com/mariusv/nginx-badbot-blocker.git
 			if [ -e "/etc/nginx/nginx-badbot-blocker/blacklist.conf" ]; then
 				getServerIPAddr
-				sed -i "s#\|python-requests##g" "/etc/nginx/nginx-badbot-blocker/blacklist.conf"
+				sed -i "s#|python-requests##g" "/etc/nginx/nginx-badbot-blocker/blacklist.conf"
 				sed -i "s#111.111.111.111#${MYIP}#g" "/etc/nginx/nginx-badbot-blocker/blacklist.conf"
 			fi
 		fi
