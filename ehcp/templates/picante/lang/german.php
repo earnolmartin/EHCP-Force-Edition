5ab6aa8a60ab382d8a18f8ee0f65c29d98fe86f5
earnolmartin <earnolmartin@gmail.com>
2021-10-26 18:32:34 -0600

    Testing German Translation

---
+++ ehcp/templates/picante/lang/german.php
@@ -0,0 +1,163 @@
+<?php
+
+// Lang definition constants:
+// Setup by Eric Arnol-Martin <earnolmartin@gmail.com>
+// German translation by Daniel Luginbuehl 27. September 2021
+
+/* 
+  #########################
+  ####Category Contants#### 
+  #########################
+*/
+
+// Domain Admin Page 
+$langDef["DOMAINADMIN_DOMAINS"] = "Domain Operationen";
+$langDef["DOMAINADMIN_FTP"] = "FTP Operationen";
+$langDef["DOMAINADMIN_MYSQL"] = "MySQL Operationen";
+$langDef["DOMAINADMIN_EMAIL"] = "E-Mail Operationen";
+$langDef["DOMAINADMIN_SETTINGS"] = "Meine Einstellungen";
+
+// Domain Admin For Reseller Page
+$langDef["FORRESELLER_DOMAINS"] = "Domain Operationen";
+$langDef["FORRESELLER_FTP"] = "FTP Operationen";
+$langDef["FORRESELLER_MYSQL"] = "MySQL Operationen";
+$langDef["FORRESELLER_EMAIL"] = "E-Mail Operationen";
+$langDef["FORRESELLER_SETTINGS"] = "Meine Einstellungen";
+
+// Email user only page
+$langDef["EMAILUSER_EMAIL"] = "E-Mail-Kontovorgänge";
+
+// DNS Only Hosting Page
+$langDef["DNSONLY_REMOTE"] = "Remote-gehostete Domänenvorgänge";
+
+// Reseller Page
+$langDef["RESELLER_RESELLER"] = "Wiederverkäufer";
+$langDef["RESELLER_DOMAINS"] = "Domain Operationen";
+$langDef["RESELLER_SYSTEM"] = "System";
+
+// Reseller Other Page
+$langDef["RESELLER_OTHER_RESELLER"] = "Wiederverkäufer";
+$langDef["RESELLER_OTHER_DOMAINS"] = "Domain Operationen";
+$langDef["RESELLER_OTHER_FTP"] = "FTP Operationen";
+$langDef["RESELLER_OTHER_EMAIL"] = "E-Mail";
+$langDef["RESELLER_OTHER_MYSQL"] = "MySQL Operationen";
+
+// Server Admin Page
+$langDef["serveradmin_users"] = "Benutzer Operationen";
+$langDef["serveradmin_domains"] = "Domain Operationen";
+$langDef["serveradmin_system"] = "System";
+
+// Server Admin Other Page                    
+$langDef["SERVERADMIN_OTHER_USERS"] = "Benutzer Operationen";
+$langDef["SERVERADMIN_OTHER_DOMAINS"] = "Domain Operations";
+$langDef["SERVERADMIN_OTHER_FTP"] = "FTP Operationen";
+$langDef["SERVERADMIN_OTHER_EMAIL"] = "E-Mail";
+$langDef["SERVERADMIN_OTHER_MYSQL"] = "MySQL Operationen";
+$langDef["SERVERADMIN_OTHER_STATUS"] = "EHCP Server Status &amp; Info";
+                                                                    
+/* 
+  ############################
+  ####Operations Contants##### 
+  ############################
+*/
+
+$langDef["ADDPANELUSER"] = "Neuen Benutzer oder Client hinzufügen";
+$langDef["EASYINSTALLSCRIPTS"] = "Easy Installationsskripte";
+$langDef["RELOADDNS"] = "DNS-Zonen neu laden";
+$langDef["SYNCDOMAINS"] = "Domänen synchronisieren";
+$langDef["CHANGEMYPASS"] = "Mein Passwort ändern";
+$langDef["UPDATEDISKQUOTA"] = "Datenträgerkontingent aktualisieren";
+$langDef["LISTUSERS"] = "Alle Benutzer auflisten";
+
+// To Test if working:
+$langDef["CONTACTUS"] = "Schreibe uns eine E-Mail";
+$langDef["TODOLIST"] = "Aufgabenliste";
+$langDef["HELP"] = "Allgemeine Hilfe";
+$langDef["ADDUSERDOMAIN"] = "Benutzer erstellen und Domäne hinzufügen";
+$langDef["ADDUSERDOMAINEASY"] = "Einfaches Benutzer erstellen und Domäne hinzufügen";
+$langDef["ADDDOMAINTOTHISUSER"] = "Domain zu meinem Konto hinzufügen";
+$langDef["ADDDOMAINBULK"] = "Bulk-Domains zu meinem Konto hinzufügen";
+$langDef["ADDDOMAINDNS"] = "DNS-Domain hinzufügen";
+$langDef["ADDDOMAINDNSTOUSER"] = "DNS-Domäne zu Benutzer hinzufügen";
+$langDef["MANAGESUBDOMAINS"] = "Subdomains verwalten";
+$langDef["PASSWORDDIRECTORY"] = "Passwortgeschützte Verzeichnisse";
+$langDef["CUSTOMDNS"] = "Benutzerdefinierte DNS";
+$langDef["CUSTOMHTTP"] = "Benutzerdefiniertes HTTP";
+$langDef["LISTDOMAINS"] = "Domains auflisten";
+$langDef["SELECTDOMAINTOMANAGE"] = "Wähle die zu verwaltende Domäne aus";
+$langDef["LISTDOMAINSPASSIVE"] = "Passive Domains auflisten";
+$langDef["ADDFTPACCOUNT"] = "FTP-Konto hinzufügen";
+$langDef["LISTFTPACCOUNTS"] = "FTP-Konten auflisten";
+$langDef["ADDFTPACCOUNTTOUSER"] = "Zeige &amp; Zuweisen von FTP-Konten zu Benutzern";
+$langDef["ADDFTPACCOUNTGENERAL"] = "Allgemeiner FTP-Benutzer hinzufügen";
+$langDef["LISTFTPACCOUNTSBYDOMAIN"] = "FTP-Benutzer nach Domäne auflisten";
+$langDef["ADDFTPSUBDIR"] = "Unterverzeichnis zu FTP-Konto hinzufügen";
+$langDef["ADDFTPSUBDOMAIN"] = "Subdomain hinzufügen und FTP-Konto erstellen";
+$langDef["LISTFTPNODOMAIN"] = "FTP-Benutzer ohne Domain auflisten";
+$langDef["ADDEMAILACCOUNT"] = "E-Mail-Konto hinzufügen";
+$langDef["LISTEMAILACCOUNTS"] = "Alle E-Mail-Benutzer auflisten";
+$langDef["LISTEMAILACCOUNTSDOMAIN"] = "Domain-E-Mail-Benutzer auflisten";
+$langDef["LISTEMAILFORWARD"] = "Zeige &amp; E-Mail-Weiterleitungen hinzufügen";
+$langDef["ADDMYSQLDB"] = "MySQL-Datenbanken hinzufügen";
+$langDef["LISTMYSQLDB"] = "MySQL-Datenbanken auflisten";
+$langDef["ADDMYSQLDBTOUSER"] = "MySQL-Datenbank zu Benutzer hinzufügen";
+$langDef["ADDMYSQLUSER"] = "MySQL-Benutzer erstellen";
+$langDef["SERVEROPERATIONS"] = "Daemon Operationen";
+$langDef["SERVERBACKUP"] = "Erstelle Backup";
+$langDef["SERVERBACKUPRESTORE"] = "Backup wiederherstellen";
+$langDef["SERVERBACKUPLIST"] = "Backups auflisten";
+$langDef["SERVEROPTIONS"] = "Optionen";
+$langDef["CHANGETHEME"] = "Thema ändern";
+$langDef["SERVERSTATUS"] = "Server Status";
+$langDef["DEFAULTDOMAIN"] = "Als Standarddomäne festlegen";
+$langDef["DELETEDOMAIN"] = "Lösche Domain";
+$langDef["WEBSTATS"] = "Statistik (Webstats)";
+$langDef["DOMAINREDIRECT"] = "Domains umleiten";
+$langDef["DOMAINTRANSFER"] = "Domain an einen anderen Benutzer übertragen";
+$langDef["DOMAINEDIT"] = "Domain aktivieren oder deaktivieren";
+$langDef["DOMAINSETTINGS"] = "Domain Einstellungen";
+$langDef["PHPMYADMIN"] = "PHPMyAdmin";
+$langDef["NET2FTP"] = "Dateimanager";
+$langDef["CHANGEFTP"] = "Ändere FTP Passwort";
+$langDef["DOMAINALIAS"] = "Aliase bearbeiten";
+$langDef["DOMAINAPACHE"] = "Apache-Vorlage bearbeiten";
+$langDef["DOMAINDNS"] = "DNS-Vorlage bearbeiten";
+$langDef["DOMAINSLAVEDNS"] = "Als Slave-DNS-Domain festlegen";
+$langDef["DOMAINSLAVEDNSREMOVE"] = "Als Master-DNS-Domain festlegen";
+$langDef["ADDSUBDOMAIN"] = "Subdomain hinzufügen";
+$langDef["LISTSUBDOMAINS"] = "Subdomains auflisten / löschen";
+$langDef["ADDEMAILFORWARDERS"] = "E-Mail-Weiterleitungen hinzufügen";
+$langDef["EDITEMAILPASSWORD"] = "E-Mail-Passwort ändern";
+$langDef["EDITEMAILAUTOREPLY"] = "Automatische Antwortnachricht ändern";
+$langDef["EMAILFORWARDINGSSELF"] = "Weiterleitungen auflisten und hinzufügen";
+$langDef["DOMAINSERVERIP"] = "IP des Domainservers ändern";
+
+$langDef["ADDCUSTOMFTP"] = "Benutzerdefiniertes FTP-Konto hinzufügen";
+$langDef["REMOVECUSTOMFTP"] = "Benutzerdefinierte(s) FTP-Konto(Konten) entfernen";
+
+$langDef["ADDCUSTOMFTP"] = "Benutzerdefiniertes FTP-Konto hinzufügen";
+$langDef["REMOVECUSTOMFTP"] = "Benutzerdefinierte(s) FTP-Konto(Konten) entfernen";
+
+$langDef["ADDREMOTEBACKUP"] = "Remote-Backup planen";
+$langDef["REMOVEREMOTEBACKUP"] = "Remote-Backups verwalten";
+$langDef["ADDCRONJOB"] = "Server-Cronjob hinzufügen";
+$langDef["REMOVECRONJOB"] = "Server-Cronjob entfernen";
+
+// Added and needed in May 2019
+$langDef["RMPASSWORDPROTECTEDDIR"] = "Passwortgeschützte Verzeichnisse entfernen/auflisten";
+$langDef["ADDPASSWORDPROTECTEDDIR"] = "Passwortgeschützte Verzeichnisse hinzufügen";
+$langDef["ADDDOMAINSSLCERT"] = "SSL-Zertifikat hinzufügen";
+$langDef["ADDSCRIPT"] = "Easy Installationsskripte";
+$langDef["ADDHOSTINGPLAN"] = "Vorlage für Hostingplan hinzufügen";
+$langDef["REMOVEHOSTINGPLAN"] = "Hostingplan-Vorlage(n) entfernen";
+$langDef["MANAGEGLOBALWEBTEMPLATES"] = "Globale Webserver-Vorlagen verwalten";
+$langDef["ADDPANELUSERWITHPREDEFINEDPLAN"] = "Neuen Benutzer w/ Hosting-Vorlage hinzufügen";
+
+// Operation Types
+$langDef["ADD"] = "Hinzufügen";
+$langDef["SYSTEM"] = "System";
+$langDef["EDIT"] = "Bearbeiten";
+$langDef["LIST"] = "Auflisten";
+$langDef["REMOVE"] = "Entfernen";
+$langDef["MANAGE"] = "Verwalten";
+?>
