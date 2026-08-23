6ccc93a3382a4dc976ffe3d946a9c7a17a02cad5
earnolmartin <earnolmartin@gmail.com>
2026-08-22 18:47:07 -0600

    EZ Installer PHPBB3 Fixes for Installer / Update Operations

--- ehcp/classapp.php
+++ ehcp/classapp.php
@@ -17145,8 +17145,14 @@ sudo service ehcp start <br>
 					break;
 					
 				case "phpbb33":
+					$custtomHttpThree = '';
+				
 					if ($directory == ''){
 						$customhttp = 'try_files_main $uri $uri/ /app.php$is_args$args;';
+						$custtomHttpThree = '
+							location /install/ { 
+								try_files $uri $uri/ /install/app.php$is_args$args; 
+							}';
 					}					
 					
 					$customHttpTwo = "";
@@ -17158,6 +17164,10 @@ sudo service ehcp start <br>
 								try_files $uri $uri/ ' . $customHttpDir . '/app.php$is_args$args;
 							}
 						';
+						$custtomHttpThree = '
+							location ' . $customHttpDir . '/install/ { 
+								try_files $uri $uri/ ' . $customHttpDir . '/install/app.php$is_args$args; 
+							}';
 					}
 					
 					
@@ -17178,6 +17188,17 @@ sudo service ehcp start <br>
 						$rs = $this->query($SQL);
 						if($rs === false || count($rs) == 0){ 
 							$success = $this->executeQuery("insert into " . $this->conf['customstable']['tablename'] . " (domainname,name,value,comment,webservertype) values ('$domainname','customhttp','" . $this->escape($customHttpTwo) . "','$comment','" . $this->miscconfig['webservertype'] . "')", 'add custom http');
+						}
+						$success = $success && $this->addDaemonOp("syncdomains", 'xx', $domainname, '', 'sync domains');
+					}
+					
+					if(isset($custtomHttpThree) && !empty($custtomHttpThree)){
+					
+						$comment = "PHPBB3.3.X Install Dir NGINX";
+						$SQL = "SELECT * FROM " . $this->conf['customstable']['tablename'] . " WHERE name = 'customhttp' and domainname = '" . $domainname . "' and comment = '" . $comment . "'";
+						$rs = $this->query($SQL);
+						if($rs === false || count($rs) == 0){ 
+							$success = $this->executeQuery("insert into " . $this->conf['customstable']['tablename'] . " (domainname,name,value,comment,webservertype) values ('$domainname','customhttp','" . $this->escape($custtomHttpThree) . "','$comment','" . $this->miscconfig['webservertype'] . "')", 'add custom http');
 						}
 						$success = $success && $this->addDaemonOp("syncdomains", 'xx', $domainname, '', 'sync domains');
 					}
